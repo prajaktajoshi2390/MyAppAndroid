64757a99492a0ffc634c6bb1a4906a93dea4e8a7
prajaktajoshi2390 <70183176+prajaktajoshi2390@users.noreply.github.com>
2021-04-07 19:32:12 -0700

    Updated Card UI and added news data

--- app/src/main/java/sjsu/cmpelkk/myappandroid/DataItem.kt
+++ app/src/main/java/sjsu/cmpelkk/myappandroid/DataItem.kt
@@ -23,12 +23,26 @@ data class DataItem(
 //    )
 
 public val carddefaultdata = listOf<DataItem>(
-        DataItem("name1", "testtitle1", "Test story, Test storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu1).toString()),
-        DataItem("name2", "testtitle2", "Test story", true, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu1).toString()),//Uri is not serializable
-        DataItem("name3", "testtitle3", "Test story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu2).toString()),
-        DataItem("name4", "testtitle4", "Test story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu3).toString())
+        DataItem("name1", "Tesla and GameStop were some of the most popular US shares last week",
+                "Tesla, Palantir and Amazon.com were some of the most popular US shares that ASX inevstors were trading last week. Here's the tea\\nThe post Tesla and GameStop were some of the most popular US shares last week appeared first on The Motley Fool Australia.",
+                false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.usshare).toString()),
+        // DataItem("name1", "testtitle1", "Test story, Test storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest storyTest story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu1).toString()),
+        DataItem("name2", "Chevrolet will build an electric Silverado with 400 miles of range - Engadget",
+                "General Motors President Mark Reuss has announced the electric Silverado pickup.", false, 4,
+                Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.silv).toString()),
+        DataItem("name3", "Patreon triples valuation to \$4 billion in new raise",
+                "Patreon has tripled its valuation to \$4 billion in a \$155 million funding round led by Tiger Global, the company confirmed to the Wall Street Journal on Tuesday.  The creator economy platform, which allows artists to be directly funded by their fans, received…", true,
+                4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.patreon).toString()),//Uri is not serializable
+
+        DataItem("name4", "U.K. Launches New Competition Watchdog Targeting Big Tech - The Wall Street Journal",
+                "Digital Markets Unit to police how largest global technology firms wield their market power", false,
+                4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.person).toString())
 )
 public val cardworlddata = listOf<DataItem>(
-    DataItem("name1", "testtitle1", "Test story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu2).toString()),
-    DataItem("name2", "testtitle2", "Test story", false, 4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.sjsu3).toString())
+    DataItem("name1", "U.K. Launches New Competition Watchdog Targeting Big Tech - The Wall Street Journal",
+            "Digital Markets Unit to police how largest global technology firms wield their market power", false,
+            4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.person).toString()),
+    DataItem("name2", "Japanese Governor Warns That Region's Hospitals Are 'on the Verge of Collapse' Amid COVID Spike",
+            "The governor of Osaka, Japan recently warned that the region's hospitals are on the \"verge of collapse\" amid a surge in COVID-19 cases.", false,
+            4, Uri.parse("android.resource://sjsu.cmpelkk.myappandroid" + "/" + R.drawable.japan).toString())
 )
